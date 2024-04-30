74d843d4a3daf9765c2a0987a13d02b577cd2f78
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-04-30 02:38:57 +0000

    created 2d10

--- dice.rb
+++ dice.rb
@@ -1,9 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "Hello World"
-end
+
 
 get("/zebra") do
   "We must add a route for each path we want to support"
@@ -11,4 +9,25 @@ end
 
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+  outcome_dice_2_10 = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d10</h1>
+  <p>#{outcome_dice_2_10}</p>"
 end
