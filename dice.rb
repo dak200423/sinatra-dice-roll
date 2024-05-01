92a62eeb16c3408f2ed9efefc274a0137e609ae2
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-01 03:08:55 +0000

    put html in erb and made connection

--- dice.rb
+++ dice.rb
@@ -63,11 +63,5 @@ outcome_dice_5_4 = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, 
 end
 
 get("/") do
-"<h1>Dice Roll</h1>
-<ul>
-  <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-  <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-  <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-  <li><a href=\"/dice/5/4\">Roll five four-sided dice</a></li>
-</ul>"
+  erb(:elephant)
 end
