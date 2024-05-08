05e853bd42512393dc741e09e23ffefc3228db1c
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-08 00:11:35 +0000

    created layout wrapper

--- dice.rb
+++ dice.rb
@@ -24,7 +24,7 @@ get("/dice/2/6") do
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  erb(:two_six)
+  erb(:two_six, { :layout => :wrapper })
 end
 
 get("/dice/2/10") do
@@ -33,14 +33,14 @@ get("/dice/2/10") do
   sum = first_die + second_die
   @outcome_dice_2_10 = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  erb(:two_ten)
+  erb(:two_ten, { :layout => :wrapper })
 end
 
 get("/dice/1/20") do
   die = rand(1..20)
   @outcome_dice_1_20 = "You rolled a #{die}."
 
-  erb(:one_twenty)
+  erb(:one_twenty, { :layout => :wrapper })
 end
 
 
@@ -54,10 +54,10 @@ sum_dice_5_4 = first_die + second_die + third_die + fourth_die + fifth_die
 
 @outcome_dice_5_4 = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die} and a #{fifth_die} for a total of #{sum_dice_5_4}."
 
-erb(:five_four)
+erb(:five_four, { :layout => :wrapper })
 
 end
 
 get("/") do
-  erb(:elephant)
+  erb(:elephant, { :layout => :wrapper })
 end
